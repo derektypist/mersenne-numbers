bedad07dae0039104686e3dccec344532e24a6e5
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-02-12 11:19:49 +0000

    Updated script.js

--- js/script.js
+++ js/script.js
@@ -23,7 +23,8 @@ function getNumberInfo() {
 
 // Function to Check if the Number is a Mersenne
 function isMersenne(num) {
-   return num.toString(2).indexOf("0") !== -1 ? false : true;
+   let bin = num.toString(2);
+   return bin.includes("0") ? false : true;
 }
 
 // Function to Clear Information
